c8460a2583d39a366a63b8a68ee9d701a8c06fc8
HP <odiljontoirjonov350@gmail.com>
2023-05-25 15:41:34 +0500

    adding and correcting a test

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,26 @@
-from django.test import TestCase
+from audioop import reverse
 
-# Create your tests here.
+from django.test import TestCase, Client
+
+from users.models import User
+
+client = Client()
+
+
+class TestUser(TestCase):
+    def setUp(self) -> None:
+        self.user = User.objects.create(
+            username='New username 1',
+            email='admin@gmail.com',
+            phone='+123456789012',
+            job='Developer',
+            adress='Tashkent',
+            birth_date='12/12/22',
+            age='16',
+            type='teacher'
+        )
+
+    def test_list_user(self):
+        url = reverse('test-user-list')
+        response = client.get(url)
+        self.assertEqual(response.status_code, 200)
